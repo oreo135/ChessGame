be6b0f5e235906a0274e3959ffc0542f35441b10
vladi <vladislavmarchenko08@gmail.com>
2024-08-21 00:52:41 +0500

    the board controller has been checked, on 5517 batches from personal all tests have passed the check. The function repetition_three_times has been added which monitors the threefold repetition 76 / 5 000 board controller checked, on 5517 batches from lichess all tests passed the check

--- ilchess/controller/board_controller.py
+++ ilchess/controller/board_controller.py
@@ -1,4 +1,5 @@
 import copy
+import hashlib
 from ilchess.state import default_state
 from collections import defaultdict
 
@@ -62,14 +63,22 @@ def _get_my_color(move_history):
 def _move_is_possible(move, game_state, move_history):
     updated_game_state = copy.deepcopy(game_state)
     updated_move_history = move_history[:] + [move]
+    start_pos, end_pos = move[0], move[1]
     start_row, start_col = move[0]
     end_row, end_col = move[1]
+    is_pawn_attack = game_state[start_row][start_col].lower() == 'p' and _is_pawn_attack(start_pos, end_pos)
+    is_pawn_cut = is_pawn_attack and _is_empty_field(game_state[end_row][end_col])
+
+    if is_pawn_cut:
+        updated_game_state[start_row][end_col] = '0'
+
     updated_game_state[start_row][start_col] = '0'
     updated_game_state[end_row][end_col] = game_state[start_row][start_col]
     w_pos, b_pos = _get_figures_positions(updated_game_state)
     enemy_possible_moves = _get_available_moves_for_state(updated_game_state, updated_move_history, evaluate_checks=False)
     our_king = w_pos['k'][0] if _get_my_color(move_history) == 'w' else b_pos['K'][0]
     is_king_castling = game_state[start_row][start_col].lower() == 'k' and abs(end_col - start_col) == 2
+
     for enemy_fig in enemy_possible_moves:
         for enemy_move in enemy_fig[1]:
             if our_king == enemy_move:
@@ -98,16 +107,20 @@ def _get_pawn_moves(pos, game_state, move_history):
         # E7E5
         if row == 6 and game_state[row - 2][col] == '0' and game_state[row - 1][col] == '0' and (row - 2) >= 0:
             moves.append((row - 2, col))
-        # E4D3 if white last was pawn D2D4
         if len(move_history):
-            if row == 3 and (col - 1) >= 0 and game_state[row][col - 1] == 'p' and move_history[-1][0] == (row - 2, col - 1)\
-                    and move_history[-1][1] == (row, col - 1):
+            # E4D3 if white last was pawn D2D4
+            if (row == 3 and (col - 1) >= 0 and
+                    game_state[row][col - 1] == 'p' and
+                    move_history[-1][0] == (row - 2, col - 1) and
+                    move_history[-1][1] == (row, col - 1)):
                 moves.append((row - 1, col - 1))
             # E4F3 if white last was pawn F2F4
-            if row == 3 and (col + 1) < 8 and game_state[row][col + 1] == 'p' and move_history[-1][0] == (row - 2, col + 1)\
-                    and move_history[-1][1] == (row, col + 1):
+            if (row == 3 and (col + 1) < 8 and
+                    game_state[row][col + 1] == 'p' and
+                    move_history[-1][0] == (row - 2, col + 1) and
+                    move_history[-1][1] == (row, col + 1)):
                 moves.append((row - 1, col + 1))
-            if _is_opposing(my_color, game_state[row - 1][col - 1]):
+            if _is_opposing(my_color, game_state[row - 1][col - 1]) and col > 0:
                 moves.append((row - 1, col - 1))
         if col < 7 and _is_opposing(my_color, game_state[row - 1][col + 1]):
             moves.append((row - 1, col + 1))
@@ -120,12 +133,16 @@ def _get_pawn_moves(pos, game_state, move_history):
             moves.append((row + 2, col))
         # E4D5
         if len(move_history) and row < 7:
-            if row == 4 and (col - 1) >= 0 and game_state[row][col - 1] == 'P' and move_history[-1][0] == (row + 2, col - 1) \
-                    and move_history[-1][1] == (row, col - 1):
+            if (row == 4 and (col - 1) >= 0 and
+                    game_state[row][col - 1] == 'P' and
+                    move_history[-1][0] == (row + 2, col - 1) and
+                    move_history[-1][1] == (row, col - 1)):
                 moves.append((row + 1, col - 1))
             # E4F5
-            if row == 4 and (col + 1) < 8 and game_state[row][col + 1] == 'P' and move_history[-1][0] == (row + 2, col + 1) \
-                    and move_history[-1][1] == (row, col + 1):
+            if (row == 4 and (col + 1) < 8 and
+                    game_state[row][col + 1] == 'P' and
+                    move_history[-1][0] == (row + 2, col + 1) and
+                    move_history[-1][1] == (row, col + 1)):
                 moves.append((row + 1, col + 1))
             if _is_opposing(my_color, game_state[row + 1][col - 1]) and col > 0:
                 moves.append((row + 1, col - 1))
@@ -421,6 +438,7 @@ def validate_moves_history(portable_moves_history):
     # translation from normal notation to algebraic,
     # where each move is in an array portable_moves_history
     portable_moves_history = portable_moves_history.split()
+    exodus = portable_moves_history[-1]
     moves = [move for index, move in enumerate(portable_moves_history[:-1]) if index % 3 != 0]
     algebraic_notation = []
     game_state = copy.deepcopy(default_state)
@@ -478,9 +496,9 @@ def validate_moves_history(portable_moves_history):
         else:
             game_state[end_row][end_col] = game_state[start_row][start_col]
         game_state[start_row][start_col] = '0'
-    print(algebraic_notation)
-    print('length algebraic notation: ', len(algebraic_notation))
-    return algebraic_notation, pawn_promotion
+    # print(algebraic_notation)
+    # print('length algebraic notation: ', len(algebraic_notation))
+    return algebraic_notation, pawn_promotion, exodus
 
 
 def _event_check(index, start_pos, end_pos, board_state, move_history, draw_move_50, draw_move_3, check_promo=True):
@@ -512,7 +530,7 @@ def _event_check(index, start_pos, end_pos, board_state, move_history, draw_move
         event += '+'
         _check = True
     if len(w_pos) == 1 and len(b_pos) == 1:
-        event = '1/2-1/2'
+        event = '1/2'
     # Function to checks events
     if has_no_moves(new_available_moves):
         if _check:
@@ -523,7 +541,7 @@ def _event_check(index, start_pos, end_pos, board_state, move_history, draw_move
                 event = '#'
         else:
             print("STALEMATE")
-            event = '1/2-1/2'
+            event = '1/2'
     if _check_draw(start_pos, end_pos, board_state):
         draw_move_50 += 1
         if draw_move_50 == 50:
@@ -531,17 +549,7 @@ def _event_check(index, start_pos, end_pos, board_state, move_history, draw_move
             event = '1/2-1/2'
     else:
         draw_move_50 = 0
-    if len(move_history) > 4 and (index + 5) <= len(move_history):
-        if move_history[index] == move_history[index + 4]:
-            last_b_fig_pos = move_history[index + 2][1]
-            last_w_fig_pos = move_history[index + 1][0]
-            if len(move_history) % 2:
-                b_fig_pos = move_history[index + 4][1]
-                w_fig_pos = move_history[index + 5][0]
-                if w_fig_pos == last_w_fig_pos or b_fig_pos == last_b_fig_pos:
-                    event = '1/2-1/2'
-        else:
-            draw_move_3 = 0
+
     return board_state, event, draw_move_3, draw_move_50, new_available_moves
 
 
@@ -562,29 +570,159 @@ def treatment(standard_notation):
     return result
 
 
-def to_standard_notation(moves_history, pawn_promotion):
+def check_enpassant(row, col, game_state, move_history):
+    my_color = 'b' if len(move_history) % 2 else 'w'
+    if my_color == 'w':
+        if (row == 4 and (col - 1) >= 0 and
+                game_state[row][col - 1] == 'P' and
+                move_history[-1][0] == (row + 2, col - 1) and
+                move_history[-1][1] == (row, col - 1)):
+            return True
+    # E4F5
+        if (row == 4 and (col + 1) < 8 and
+                game_state[row][col + 1] == 'P' and
+                move_history[-1][0] == (row + 2, col + 1) and
+                move_history[-1][1] == (row, col + 1)):
+            return True
+    else:
+        if (row == 3 and (col - 1) >= 0 and
+                game_state[row][col - 1] == 'p' and
+                move_history[-1][0] == (row - 2, col - 1) and
+                move_history[-1][1] == (row, col - 1)):
+            return True
+    # E4F3 if white last was pawn F2F4
+        if (row == 3 and (col + 1) < 8 and
+                game_state[row][col + 1] == 'p' and
+                move_history[-1][0] == (row - 2, col + 1) and
+                move_history[-1][1] == (row, col + 1)):
+            return True
+    return False
+
+
+def repetition_three_times(move, current_state, moves_history, dict_states, copy_dict, w_pos, b_pos, number):
+    enpassant = []
+    castling = ''
+    direction = int
+    lens_state = len(current_state)
+    white_castling = _get_possible_castlings(w_pos['k'][0], current_state, moves_history)
+    black_castling = _get_possible_castlings(b_pos['K'][0], current_state, moves_history)
+    # w_move = _get_available_moves(w_pos, current_state, moves_history)
+    # b_move = _get_available_moves(b_pos, current_state, moves_history)
+    # print('current_string', string)
+    new_current = ''
+
+    for b_pawn_pos in b_pos['P']:
+        row, col = b_pawn_pos
+        if check_enpassant(row, col, current_state, moves_history):
+            enpassant.append(b_pawn_pos)
+    for w_pawn_pos in w_pos['p']:
+        row, col = w_pawn_pos
+        if check_enpassant(row, col, current_state, moves_history):
+            enpassant.append(w_pawn_pos)
+
+    # It is necessary to check the order of the move according to
+    # the current history of moves.
+    # If the number is even according order move - white in code - 0
+    direction = 1 if lens_state % 2 else 0
+
+    for pos in white_castling:
+        if pos[1] == 5:
+            castling += 'q'
+        else:
+            castling += 'k'
+
+    for pos in black_castling:
+        if pos[1] == 5:
+            castling += 'Q'
+        else:
+            castling += 'K'
+
+    string = str(direction) + str(enpassant) + castling + str(current_state)
+    print('string', string)
+
+    if string in dict_states:
+        dict_states[string] += 1
+        copy_dict[string].append(number+1)
+        if dict_states[string] == 3:
+            print("work")
+            return 3
+
+    else:
+        copy_dict[string] = []
+        dict_states[string] = 1
+    for row in current_state:
+        row = str(row) + '\n'
+        new_current += row
+    print('move', move)
+    print(f'copy_dic_number = {copy_dict[string]}')
+    print(f'repeat: {dict_states[string]} state:\n{new_current}\n')
+
+    return copy_dict, dict_states
+
+
+def not_enough_pieces(board_state):
+    w_pos, b_pos = _get_figures_positions(board_state)
+    black_fig, b_bishop_color = [], None
+    white_fig, w_bishop_color = [], None
+    check_white = True
+    check_black = True
+    for fig, pos in w_pos.items():
+        if (fig == 'n' and len(pos) == 1) or (fig == 'b' and len(pos)) == 1 or fig == 'k':
+            white_fig.append(fig)
+            if fig == 'b':
+                row, col = w_pos[fig][0]
+                white_bishop_color = 'b' if (row + col) % 2 else 'w'
+        else:
+            check_white = False
+            break
+    for fig, pos in b_pos.items():
+        if (fig == 'N' and len(pos) == 1) or (fig == 'B' and len(pos)) == 1 or fig == 'K':
+            black_fig.append(fig)
+            if fig == 'b':
+                row, col = b_pos[fig][0]
+                black_bishop_color = 'b' if (row + col) % 2 else 'w'
+        else:
+            check_black = False
+            break
+    white_fig = list(map(lambda x: x.upper, white_fig))
+    if check_white and check_black:
+        if abs(len(white_fig) - len(black_fig)) == 1:
+            return True
+        elif white_fig == black_fig and b_bishop_color == w_bishop_color:
+            return True
+    else:
+        return False
+
+
+def to_standard_notation(moves_history, pawn_promotion, exodus=None):
     current_state = copy.deepcopy(default_state)
-    next_state = copy.deepcopy(default_state)
+    last_state = copy.deepcopy(default_state)
     standard_notation = []
     top_labels = ["a", "b", "c", "d", "e", "f", "g", "h"][::-1]
-    count_promo = 1
     current_move_history = []
     draw_move_50 = 0
     draw_move_3 = 0
+    dict_states = {}
+    copy_dict = {}
+    number_state = {}
+    repetition = False
 
     for index, (start_pos, end_pos) in enumerate(moves_history):
         end_row, end_col = end_pos
         start_row, start_col = start_pos
         fig = current_state[start_row][start_col]
-        color = 'w' if index % 2 == 0 else 'b'
-        commons_moves_algebraic = []
-        w_pos, b_pos = _get_figures_positions(current_state)
-        player_positions = w_pos if color == 'w' else b_pos
         move_in_standard = ''
         check_promo = True
-        print('number_move:', index)
+        length_history = len(moves_history)
+        commons_moves_algebraic = []
+
+        color = 'w' if index % 2 == 0 else 'b'
+        w_pos, b_pos = _get_figures_positions(current_state)
+        player_positions = w_pos if color == 'w' else b_pos
+        print('number_move:', index+1)
         is_pawn_attack = current_state[start_row][start_col].lower() == 'p' and _is_pawn_attack(start_pos, end_pos)
         is_pawn_cut = is_pawn_attack and _is_empty_field(current_state[end_row][end_col])
+
         if (end_row == 7 or end_row == 0) and fig.lower() == 'p':
             if current_state[end_row][end_col] != '0':
                 move_in_standard = f'{top_labels[start_col]}x{top_labels[end_col]}{end_row + 1}'
@@ -639,7 +777,7 @@ def to_standard_notation(moves_history, pawn_promotion):
                     if check_row and not check_col:
                         move_in_standard = f'{top_labels[start_col]}x{top_labels[end_col]}{end_row + 1}'
                     elif not check_row and check_col:
-                        move_in_standard = f'{start_row + 1}{top_labels[end_col]}x{end_row + 1}'
+                        move_in_standard = f'{top_labels[start_col]}{start_row + 1}x{top_labels[end_col]}{end_row + 1}'
                     elif not check_row and not check_col and len(commons_moves_algebraic) >= 1:
                         move_in_standard = f'{top_labels[start_col]}x{top_labels[end_col]}{end_row + 1}'
                     else:
@@ -655,26 +793,80 @@ def to_standard_notation(moves_history, pawn_promotion):
                                                                                             draw_move_50,
                                                                                             draw_move_3,
                                                                                             check_promo)
+        # if _check_remove_piece((start_pos, end_pos), last_state) or is_pawn_cut\
+        #         or not check_promo\
+        #         or event in {'O-O', 'O-O-O'}:
+        #     copy_dict, dict_states = repetition_three_times(move_in_standard, current_state,
+        #                                                     current_move_history,{},
+        #                                                     {}, w_pos, b_pos, index)
+        #
+        # else:
+        #     result_rep_func = repetition_three_times(move_in_standard, current_state, current_move_history, dict_states,
+        #                                                     copy_dict, w_pos, b_pos, index)
+        #
+        #     if result_rep_func == 3:
+        #         repetition = True
+        #     else:
+        #         copy_dict, dict_states = result_rep_func
+
+
+        last_state = copy.deepcopy(current_state)
+        check_enough_piece = not_enough_pieces(current_state)
+        fig = fig if fig.lower() != 'p' else ''
+        number_state[index+1] = current_state
 
         if event in {'O-O', 'O-O-O'}:
             move_in_standard = event
         else:
-            if (index + 1) in pawn_promotion:
+            if ((index + 1) in pawn_promotion and
+                    length_history == index + 1 and
+                    '1/2' in exodus and '1/2' not in event):
+
                 fig = f'={pawn_promotion[index + 1].upper()}'
-                move_in_standard = move_in_standard + fig + event
-            elif fig.lower() != 'p' and not any(x in event for x in {'O-O', '1/2'}):
-                move_in_standard = fig.upper() + move_in_standard + event
-            elif fig.lower() != 'p' and '1/2' in event and color == 'w':
+                move_in_standard = move_in_standard + fig.upper() + event + ' 1/2' if color == 'w' \
+                    else fig.upper() + move_in_standard + '' + event
+
+            elif (index + 1) in pawn_promotion:
+
+                fig = f'={pawn_promotion[index + 1].upper()}'
+                if '1/2' not in event:
+                    move_in_standard = move_in_standard + fig + event
+                else:
+                    move_in_standard = move_in_standard + fig + ' 1/2' if color == 'w' \
+                    else move_in_standard + fig
+
+            elif check_enough_piece:
+
+                move_in_standard = fig.upper() + move_in_standard + event + ' 1/2' if color == 'w' \
+                             else fig.upper() + move_in_standard + '' + event
+
+            elif length_history == index + 1 and '1/2' in exodus and '1/2' not in event:
+
+                move_in_standard = (fig.upper() + move_in_standard + event) + ' 1/2' if color == 'w' \
+                    else fig.upper() + move_in_standard + '' + event
+
+            elif not any(x in event for x in {'O-O', '1/2'}):
+
+                move_in_standard = fig.upper() + move_in_standard + event if color == 'w' \
+                    else fig.upper() + move_in_standard + event
+
+            elif '1/2' in event and color == 'w':
+
                 move_in_standard = fig.upper() + move_in_standard + ' ' + '1/2'
-            elif fig.lower() != 'p' and '1/2' in event and color == 'b':
+
+            elif '1/2' in event and color == 'b':
+
                 move_in_standard = fig.upper() + move_in_standard
+
             elif 'O-O' in event:
+
                 move_in_standard = event
+
             else:
                 move_in_standard += event
-        print('move_in_standard', move_in_standard)
+
         standard_notation.append(move_in_standard)
-    print('standard_notation:', standard_notation)
+    # print('number_state', number_state)
     return standard_notation
 
 
@@ -718,6 +910,7 @@ class BoardController(object):
         self._count_move = 0
         self._check_event = {}
         self._standard_notation = []
+        self._dict_states = {}
 
     def get_available_moves(self):
         return dict(_get_available_moves_for_state(self._board_state, self._moves_history))
@@ -728,7 +921,7 @@ class BoardController(object):
             self._count_move += 1
             start_row, start_col = start_pos
             end_row, end_col = end_pos
-
+            w_pos, b_pos = _get_figures_positions(self._board_state)
             is_pawn_attack = self._board_state[start_row][start_col].lower() == 'p' and _is_pawn_attack(start_pos, end_pos)
             is_pawn_cut = is_pawn_attack and _is_empty_field(self._board_state[end_row][end_col])
             if is_pawn_cut:
@@ -748,9 +941,11 @@ class BoardController(object):
             self._board_state[end_row][end_col] = self._board_state[start_row][start_col]
             self._board_state[start_row][start_col] = '0'
             self._moves_history.append((start_pos, end_pos))
-
             new_available_moves = self.get_available_moves()
-
+            self._dict_states = repetition_three_times(self._board_state,
+                                                       self._moves_history,
+                                                       self._dict_states,
+                                                       w_pos, b_pos)
             if _is_check(self._board_state, self._moves_history):
                 self._check_event[self._count_move] = '+'
                 _check = True
--- ilchess/tests/controller_test.py
+++ ilchess/tests/controller_test.py
@@ -6,19 +6,22 @@ path = '/home/vladi/Downloads/lichess_db_standard_rated_2013-01.pgn'
 
 def read_chess_moves(filename, max_lines=1000):
     history_matches = []
-
+    way = []
     with open(filename, 'r') as file:
         for _ in range(max_lines):
             line = file.readline()
+            if '[Termination' in line:
+                lst = list(line.split())
+                way.append(lst[1])
             if '[' in line or line == '\n':
                 continue
             print(line.strip())
             history_matches.append(line.strip())
-    return history_matches
+    return history_matches, way
 
 
-test_kit = read_chess_moves(path, 10000)
-
+test_kit, ways = read_chess_moves(path, 100000)
+print('ways', ways)
 
 class ControllerTest(TestCase):
     def setUp(self):
@@ -29,10 +32,8 @@ class ControllerTest(TestCase):
         def tests(self):
             print(f'Test for index {ind}')
             print('current_game_moves', match_moves)
-            algebraic_notation, pawn_promotion = validate_moves_history(match_moves)
-            print('algebraic', algebraic_notation)
-            standard_notation = treatment(to_standard_notation(algebraic_notation, pawn_promotion))
-            print('standard_notation', standard_notation)
+            algebraic_notation, pawn_promotion, exodus = validate_moves_history(match_moves)
+            standard_notation = treatment(to_standard_notation(algebraic_notation, pawn_promotion, exodus))
             history = match_moves.split()
             if len(history) % 3 == 0:
                 expected_moves = treatment([move for count, move in enumerate(history) if count % 3 != 0])
@@ -45,25 +46,16 @@ class ControllerTest(TestCase):
                 self.assertEqual(my_move, standard_move, f'my_move {my_move} st_move{standard_move}')
         return tests
 
+for index, game_moves in enumerate(test_kit):
+    print('number_test', index)
+    print('game_moves', game_moves)
+    test_name = f'test_game_moves_{index}'
+    test = ControllerTest.generate_test(game_moves, index)
+    setattr(ControllerTest, test_name, test)
 
-# for index, game_moves in enumerate(test_kit):
-#     print('number_test', index)
-#     print('game_moves', game_moves)
-#     test_name = f'test_game_moves_{index}'
-#     test = ControllerTest.generate_test(game_moves, index)
-#     setattr(ControllerTest, test_name, test)
+# one_game_match = ('1. d4 d5 2. c4 e6 3. Nf3 Bb4+ 4. Bd2 Bxd2+ 5. Qxd2 dxc4 6. e4 Nf6 7. e5 Nd5 8. Bxc4 c6 9. O-O O-O 10. a3 b5 11. Bd3 Bb7 12. Ng5 Nd7 13. Bxh7+ Kh8 14. Qd3 Qxg5 15. Qh3 Qh6 16. Qxh6 gxh6 17. Bc2 a5 18. f4 Ne3 19. Rf2 Nxc2 20. Rxc2 Nb6 21. Rc3 Nc4 22. Rh3 Kh7 23. b3 Nb6 24. Nc3 Nd5 25. Ne4 Nxf4 26. Nf6+ Kg7 27. Rg3+ Kh8 28. Rf1 Ne2+ 29. Kh1 Nxg3+ 30. hxg3 Rg8 31. Kh2 Rad8 32. Rf4 Rd5 33. Nxg8 Kxg8 34. Rf6 Kg7 35. Rf4 a4 36. b4 Rd8 37. Kh3 Rg8 38. Kh4 Kf8 39. Kh5 Rxg3 40. Kxh6 Rxg2 41. Kh5 Rg3 42. Rh4 Rxa3 43. Kg5 Rb3 44. Kf6 Rf3+ 45. Kg5 a3 46. Kg4 Rf2 47. Kg3 Rf1 48. Rh2 Ke7 49. Ra2 Rd1 50. Rxa3 Rxd4 51. Ra7 Rd7 52. Kf4 Rd4+ 53. Ke3 Rd7 54. Ke4 Bc8 55. Ra8 Rc7 56. Rb8 Bd7 57. Rb6 c5 58. bxc5 Rxc5 59. Kd4 Rc4+ 60. Kd3 Rc5 61. Kd4 Rd5+ 62. Ke4 f5+ 63. exf6+ Kxf6 64. Rb7 Re5+ 65. Kd4 Rd5+ 66. Ke3 Bc6 67. Rh7 Rf5 68. Rh6+ Ke5 69. Rh2 Bd5 70. Re2 Bc4 71. Re1 Kd6 72. Rd1+ Rd5 73. Rb1 e5 74. Rc1 Rd4 0-1')
+# test1 = ControllerTest.generate_test(one_game_match, 1)
+# setattr(ControllerTest, 'test_my_games', test1)
 
-
-one_game_match = ('1. e4 d5 2. exd5 Qxd5 3. c4 Qd8 4. Nc3 e6 5. Nf3 Bb4 6. d4 Be7 7. d5 exd5 8. cxd5 Nf6 9'
-                  '. Qa4+ Bd7 10. Bb5 Bxb5 11. Qxb5+ c6 12. dxc6 Nxc6 13. Ne5 O-O 14. Nxc6 bxc6 15. Qxc6 Rc8 '
-                  '16. Qf3 Re8 17. O-O Bb4 18. Rd1 Qc7 19. Bf4 Qc5 20. Rac1 Qf8 21. Nd5 Nxd5 22. Qxd5 Rxc1 '
-                  '23. Rxc1 Re1+ 24. Rxe1 Bxe1 25. Bd2 Bxd2 26. Qxd2 Qb8 27. h3 h6 28. b3 Qb6 29. Qd3 Qa5 '
-                  '30. a4 Qe1+ 31. Kh2 Qe5+ 32. g3 Qb2 33. Kg2 Qf6 34. Qe3 Qa6 35. h4 Qb7+ 36. Qf3 Qb6 '
-                  '37. Qe3 Qb7+ 38. f3 Qd7 39. g4 Qc7 40. g5 hxg5 41. hxg5 Qc2+ 42. Kg3 Qc7+ 43. f4 g6 '
-                  '44. Kf3 Qb7+ 45. Kg4 Qd7+ 46. Kg3 Qb7 47. Qd3 Qb6 48. Qf3 Qg1+ 49. Qg2 Qe3+ 50. Kg4 Qxb3 '
-                  '51. Qa8+ Kg7 52. Qxa7 Qe6+ 53. Kf3 Qd5+ 54. Ke3 Qe6+ 55. Kd3 Qf5+ 56. Ke3 Qh3+ 57. Ke4 Qf5+ '
-                  '58. Ke3 Qh3+ 59. Kd4 Qf5 60. Ke3 1/2-1/2')
-test1 = ControllerTest.generate_test(one_game_match, 1)
-setattr(ControllerTest, 'test_my_games', test1)
 if __name__ == '__main__':
     main()
